048355935150e09d0673f71b455e5a86366ee839
Yutian Qin <yutianqinx@gmail.com>
2025-12-19 18:22:41 -0700

    scidx-rexec 0.0.5 release: update setup.py for package name and version; modify set_environment method to use usr_id; add MANIFEST.in for package inclusion.

--- rexec/client_api.py
+++ rexec/client_api.py
@@ -40,7 +40,7 @@ class remote_func:
         cls.rexec_api_url = url
         
     @classmethod
-    def set_environment(cls, filename, usr_token=None):
+    def set_environment(cls, filename, usr_id=None):
         requirements = []
         with open(filename, 'r') as fd:
             for line in fd:
@@ -58,20 +58,6 @@ class remote_func:
                             packaging.specifiers.Specifier(req.specifier.__str__()).operator != "==")):
                             raise packaging.requirements.InvalidRequirement(f"We only support an exact Python version specification. For example: python==3.13")
                         requirements.append(req_str)
-        if usr_token:
-            jwks_url = "https://idp-test.nationaldataplatform.org/realms/NDP/protocol/openid-connect/certs"
-            jwks_client = jwt.PyJWKClient(jwks_url)
-            signing_key = jwks_client.get_signing_key_from_jwt(usr_token)
-            decoded = jwt.decode(
-                usr_token,
-                signing_key.key,            # RSA public key
-                algorithms=["RS256"],
-                audience="account",         # matches the "aud" claim in the token
-                issuer="https://idp-test.nationaldataplatform.org/realms/NDP"
-            )
-            usr_id = decoded["sub"]
-        else:
-            raise RuntimeError("Token is required to set up the R-Exec environment.")
 
         response = requests.post(cls.rexec_api_url, data={"requirments": requirements, "user_id": usr_id})
         if response.status_code == 404:
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-setup(name='SciDx-rexec-test',
-      version='0.0.4',
+setup(name='scidx-rexec',
+      version='0.0.5',
       description='Client library for SciDX Remote Execution',
       long_description="Client library for remote execution capabilities in "
                        "SciDx software stack with the support to DataSpaces "
@@ -13,7 +13,7 @@ setup(name='SciDx-rexec-test',
                        "arbitary user-defined functions on the remote Points-of-Presence (PoPs).",
       long_description_content_type="text/x-rst",
       author='Yutian Qin',
-      author_email='yutianqinx@gmail.com',
+      author_email='yutian.qin@utah.edu',
       url='https://github.com/sci-ndp/SciDx-rexec',
       install_requires=requirements,
       packages=['rexec'],
